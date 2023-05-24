62be0fbba464019600e19154264a3bb2c301a657
cadart <cadart.valentin@sfr.fr>
2023-05-24 16:16:28 +0200

    directive challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '03-Directives';
+  title = 'Bonjour!';
+  isAdmin : boolean = true;
+
+updateIsAdmin() {
+  this.isAdmin = !this.isAdmin;
+}
 }
